a5b279c2068d79b41268b103e6617ea58dfb7eb4
aeon.b.avinash <aeon.b.avinash@gmail.com>
2024-01-26 23:33:37 +0000

    commented background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action == "get_background_color") {
-        sendResponse({ color: "#000000" });
-    }
-});
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     if (request.action == "get_background_color") {
+//         sendResponse({ color: "#000000" });
+//     }
+// });
